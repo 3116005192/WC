0bf5590b5a3e47441fc2c580fbc2895e04d44f5c
3116005192 <43093616+3116005192@users.noreply.github.com>
2018-09-15 08:26:26 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ void Char_Count_Out(int, char*[]);
 int main(int argc, char* argv[])
 {
 	if (argc == 1 || argc == 2)            //无文件名称
-		std::cout << "Not found the file_name";
+		std::cout << "\nNot found the file_name\n";
 	if (argc > 3)
 	{
-		if (argv[1] == "l")
+		if (argv[1][1] == 'l')
 			Line_Count_Out(argc, argv);
-		if (argv[1] == "w")
+		if (argv[1][1] == 'w')
 			Word_Count_Out(argc, argv);
-		if (argv[1] == "c")
+		if (argv[1][1] == 'c')
 			Char_Count_Out(argc, argv);
 	}
 	std::cout << std::endl;
@@ -30,12 +30,15 @@ void Line_Count_Out(int c, char* v[])            //计算行数并输出
 	int lines = 1;
 	for (c -= 1; c > 1; c--)
 	{
-		file_stream.open(v[c], std::ios::in);    //打开文件 
-		file_stream.read(ch, 1);                 //以字符数组读取每一个字符,每循环一次,指针向下一个字符地址移动一次
-		if (*ch == '\n') lines++;
+		file_stream.open(v[c], std::ios::in);    //打开文件
+		do
+		{
+			file_stream.read(ch, 1);             //以字符数组读取每一个字符,每循环一次,指针向下一个字符地址移动一次
+			if (*ch == '\n') lines++;
+		} while (ch == NULL);
 		file_stream.close();                     //关闭文件
 	}
-	std::cout << "The amount of line: " << lines << std::endl;
+	std::cout << std::endl << "The amount of line: " << lines << std::endl;
 }
 
 void Word_Count_Out(int c, char* v[])            //计算单词数并输出
@@ -47,22 +50,25 @@ void Word_Count_Out(int c, char* v[])            //计算单词数并输出
 	for (c -= 1; c > 1; c--)
 	{
 		file_stream.open(v[c], std::ios::in);
-		file_stream.read(ch, 1);
-		
-		//当字符串中含有的一个字符为字母时,flag = true, 反之, flag = false, 每次由false变为true时, words加一.
-		
-		if (*ch > 'A' && *ch < 'Z' || *ch > 'a' && *ch < 'z')
+		do
 		{
-			if (flag == false)
+			file_stream.read(ch, 1);
+
+			//当字符串中含有的一个字符为字母时,flag = true, 反之, flag = false, 每次由false变为true时, words加一.
+
+			if (*ch > 'A' && *ch < 'Z' || *ch > 'a' && *ch < 'z')
 			{
-				words++;
-				flag = true;
+				if (flag == false)
+				{
+					words++;
+					flag = true;
+				}
 			}
-		}
-		else flag = false;
+			else flag = false;
+		} while (ch == NULL);
 		file_stream.close();
 	}
-	std::cout << "The amount of word: " << words << std::endl;
+	std::cout << std::endl << "The amount of word: " << words << std::endl;
 }
 
 void Char_Count_Out(int c, char* v[])            //计算字符数并输出
@@ -73,9 +79,12 @@ void Char_Count_Out(int c, char* v[])            //计算字符数并输出
 	for (c -= 1; c > 1; c--)
 	{
 		file_stream.open(v[c], std::ios::in);
-		file_stream.read(ch, 1);
-		if (*ch) chars++;
-		file_stream.close();
+		do
+		{
+			file_stream.read(ch, 1);
+			if (ch != NULL) chars++;
+			file_stream.close();
+		} while (ch == NULL);
 	}
-	std::cout << "The amount of char: " << chars << std::endl;
+	std::cout << std::endl << "The amount of char: " << chars << std::endl;
 }
